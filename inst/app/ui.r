831ce888be64ac230a21d9ff2436036a02dd0090
johnchower <johnchower@gmail.com>
2017-01-04 17:31:18 -0700

    Create working Shiny app.

---
+++ inst/app/ui.r
@@ -0,0 +1,38 @@
+tidyData <- notificationGraphs::tidy_results   
+mindate <- min(tidyData$sent_week_start_date)
+maxdate <- max(tidyData$sent_week_start_date)
+event_type_choices <- as.list(unique(tidyData$event_type))
+
+fluidPage(
+  titlePanel("Email Notification Click Rates")
+  , sidebarPanel(
+    radioButtons("variable"
+                 , label = "Variable"
+                 , choices = 
+                  list("Click Rate" = "click"
+                       , "Manage Rate" = "manage"
+                       , "Unsubscribe Rate" = "unsubscribe")
+                 , selected = "click"),
+    radioButtons("chart_type"
+                 , label = "Chart Type"
+                 , choices = 
+                  list("Aggregate" = 1
+                       , "Individual" = 0)
+                 , selected = 1),
+    sliderInput("date_range"
+                , label = "Date Range"
+                , min = mindate
+                , max = maxdate
+                , step = 7
+                , value = c(mindate, maxdate)),
+    selectInput("event_type"
+                , label = "Event Type"
+                , choices = event_type_choices
+                , multiple = T
+                , selected = event_type_choices) 
+  )
+  , mainPanel(
+    textOutput('text'),          
+    plotOutput('plot')
+  )
+)
